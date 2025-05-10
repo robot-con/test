da5a4bf5062450526b6099fd58d9e175228bbe57
Shyamsundar paul <shyamsundarpaul.lic@gmail.com>
2025-05-10 21:15:48 +0530

    main.py

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ server.bind(("0.0.0.0", 10000))
 server.listen(5)
 
 def handel(conn):
-    data = conn.recv(1020)
-    if not data:
+    try:
+        data = conn.recv(1020)
+        if not data:
+            conn.close()
+        send = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n"
+        send += "<h1>this is socket server</h1>"
+        conn.sendall(send.encode())
         conn.close()
-    send = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n"
-    send += "<h1>this is socket server</h1>"
-    conn.sendall(send.encode())
-    conn.close()
+    except Exception as e:
+        conn.close()
 while True:
     try:
         conn , addr =server.accept()
