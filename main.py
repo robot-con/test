58b52c60adb35ebb93390754f256065868a30a0c
unknown <shyamsundarpaul.lic@gmail.com>
2025-05-10 21:10:22 +0530

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 10000))
+server.listen(5)
+
+def handel(conn):
+    data = conn.recv(1020)
+    if not data:
+        conn.close()
+    send = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n"
+    send += "<h1>this is socket server</h1>"
+    conn.sendall(send.encode())
+    conn.close()
+while True:
+    try:
+        conn , addr =server.accept()
+        print(addr)
+
+        thread = threading.Thread(target=handel, args=(conn,),daemon= True)
+        thread.start()
+
+    except Exception as e:
+        print (e)
+    
+
+
