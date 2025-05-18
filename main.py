5b0ffc0e956f4bb109a5f99843b2fa0e73557472
Shyamsundar paul <shyamsundarpaul.lic@gmail.com>
2025-05-18 22:07:43 +0530

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import socket
-
-server = socket.socket()
-
-server.bind(("0.0.0.0",10000))
-server.listen(5)
+import threading
 
 
-while True:
-    try:
-        conn , addr = server.accept()
-        data = conn.recv(3099)
-        if not data:
-            conn.close()
-        
-        print(data.decode())
-        conn.send("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nConnection: keep-alive\r\n\r\nTunnel connected\n".encode())
-        while True:
-            data = conn.recv(3099)
-            print(data.decode())
-            conn.send("data recevied".encode())
-
-    except Exception as e:
-        print("conn error")
+def fun_ser(conn,addr):
+    data = conn.recv(5000)
+    if not data:
         conn.close()
-    finally:
-        continue
+    print(data.decode())
+    conn.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: keep-alive\r\n\r\n  server ok".encode())
+
+    while True:
+        dt = conn.recv(5000)
+        if not dt:
+            conn.close()
+            break
+
+        print(dt.decode())
+        conn.send(f"data receive: {dt}".encode())        
+
+def main():
+    server = socket.socket()
+    server.bind(("0.0.0.0",10000))
+    server.listen(5)
+
+    while True:
+        conn , addr = server.accept()
+        handel_server = threading.Thread(target=fun_ser, args=(conn,addr),daemon= True)
+        handel_server.start()
+
+if __name__=="__main__":
+    main()
